53810513cd7317369fd6df4f5b08f38616dde014
jtrucolo <johnny.trucolo97@gmail.com>
2023-07-29 13:56:57 -0300

    Add files

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    const username = $('#name');
+    const avatar = $('#img');
+    const followers = $('#followers');
+    const following = $('#following');
+    const repository = $('#repository');
+    const profile_link = $('#profile');
+
+    console.log(username)
+
+    fetch('https://api.github.com/users/jtrucolo')
+    .then(function(result) {
+        return result.json();
+    })
+    .then(function(json) {
+        avatar.attr("src", json.avatar_url)
+        username.text(json.name)
+        followers.text(json.followers)
+        following.text(json.following)
+        repository.text(json.public_repos)
+        profile_link.attr("href", json.html_url)
+    })
+})
