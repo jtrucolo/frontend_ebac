db8f615f1470e08ba94ad8595d574835daed0452
jtrucolo <johnny.trucolo97@gmail.com>
2023-08-19 18:43:11 -0300

    Ex Typescript

---
+++ ex.ts
@@ -0,0 +1,16 @@
+function multiplica(a: number, b: number): number {
+    return a * b;
+}
+
+const multiplicador = multiplica(10,2);
+
+console.log('Resultado do calculo:', multiplicador);
+
+
+function hello(nome: string): string {
+return nome;
+}
+
+const hello_people = hello('Ebac');
+
+console.log('Hello', hello_people);
